3e90ba062eee8c6c9f9f46a167acca124e7bc8d1
atdevala <aidandeval@gmail.com>
2026-05-21 00:49:56 -0400

    Add API platform adapter services

---
+++ artifacts/api-server/src/services/index.ts
@@ -0,0 +1,6 @@
+export * from "./market-data-service";
+export * from "./normalizers";
+export * from "./registries";
+export * from "./scanner-service";
+export * from "./score-service";
+export * from "./signal-history-service";
